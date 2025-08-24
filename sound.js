c630a170f162e66651692db27dee2f204dd21908
pidoy0218-arts <pidoy0218@gmail.com>
2025-08-24 08:04:33 +0800

    Delete sound.js

--- sound.js
+++
@@ -1,6 +0,0 @@
-// Small, no-framework sound helpers
-const MOVE_SFX = "https://assets.mixkit.co/sfx/download/mixkit-hard-typewriter-click-1119.wav";
-const WIN_SFX  = "https://assets.mixkit.co/sfx/download/mixkit-winning-chimes-2015.wav";
-
-window.playMoveSound = () => new Audio(MOVE_SFX).play().catch(()=>{});
-window.playWinSound  = () => new Audio(WIN_SFX).play().catch(()=>{});
